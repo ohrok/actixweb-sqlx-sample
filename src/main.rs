9d0e642a3fcd07e03a5eb9289641df14f59b8ab4
ohrok <oh.hymh@gmail.com>
2021-11-27 21:39:01 +0900

    add actix-web and sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+
+#[actix_web::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://actix_username:actix_password@localhost:5432/actix_database")
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
 }
