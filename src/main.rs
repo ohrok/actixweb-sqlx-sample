ceda2961e49c00b4f1569f6a6fed9d04b9dc2a86
ohrok <oh.hymh@gmail.com>
2021-11-28 17:40:07 +0900

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
+use log::info;
 use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = PgPoolOptions::new()
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let db_pool = PgPoolOptions::new()
         .max_connections(5)
+        // TODO: use environment variables.
         .connect("postgres://actix_username:actix_password@localhost:5432/actix_database")
         .await?;
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
+    let server = HttpServer::new(move || {
+        App::new()
+            .data(db_pool.clone())
+            .wrap(middleware::Logger::default())
+            .route("/", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8080")?; // TODO: use environment variables.
 
-    assert_eq!(row.0, 150);
+    info!("Starting server");
+    server.run().await?;
 
     Ok(())
 }
